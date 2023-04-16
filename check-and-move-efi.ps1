c43a537eb8d2a3fbae9e0ff0a00f44c3c1b6c58c
threader <michael.heltne@gmail.com>
2023-04-16 15:40:39 +0200

    re-think logic after some sleep.

--- check-and-move-efi.ps1
+++ check-and-move-efi.ps1
@@ -6,10 +6,6 @@ Write-Output "Mounting EFI to W:"
 mountvol w: /S
 }
 
-if (-not (Test-Path "W:\EFI\Microsoft\Boot\bootmgfww.efi")) {
-Write-Output "EFI\Microsoft\Boot\bootmgfww.efi not found."
-}
-
 $msefi = 'W:\EFI\Microsoft\Boot\bootmgfw.efi'
 $checkforstring = (Get-ChildItem -Path $msefi| Select-Object -First 1).fullname
 if ($checkforstring) {
@@ -28,47 +24,6 @@ if ($checkforstring) {
     "No file: $msefi"
 }
 
-write-output "Located grub*.efi and path:"
-
-$GrubEfiFileLoc = Get-ChildItem –Path W:\EFI -include grubx64.efi*,grubia32.efi* -Force -Recurse |
-    ? FullName -notLike 'W:\EFI\Microsoft\Boot\grubx64.efi*' |
-        ? FullName -notLike 'W:\EFI\Microsoft\Boot\grubia32.efi*' |
-        Get-ChildItem -File -Force |
-            select-object -Expand FullName
-$GrubEfiFileLoc 
-
-$GrubPwd = Split-Path -Path "$GrubEfiFileLoc" -Parent
-write-output "GRUB location: $GrubPwd"
-
-
-if (Test-Path "$GrubPwd\grubia32.efi*") {
-
-$MsGrubEfiLoc = "W:\EFI\Microsoft\Boot\grubia32.efi"
-write-output "GRUB EFI is 32bit using: $MsGrubEfiLoc"
-} else {
-$MsGrubEfiLoc = "W:\EFI\Microsoft\Boot\grubx64.efi"
-write-output "GRUB EFI is 64bit using: $MsGrubEfiLoc"
-}
-
-if((Get-FileHash $GrubEfiFileLoc).hash  -ne (Get-FileHash W:\EFI\Microsoft\Boot\bootmgfw.efi).hash) {
-Write-Output "$GrubEfiFileLoc is not the bootmgfw.efi"
- cp $GrubPwd W:\EFI\Microsoft\Boot\
-} else { 
-Write-Output "$GrubEfiFileLoc is the bootmgfw.efi"
-}
-
-if((Get-FileHash $GrubEfiFileLoc).hash  -ne (Get-FileHash $MsGrubEfiLoc).hash) {
-Write-Output "$GrubEfiFileLoc is not the same as $MsGrubEfiLoc"
- cp $GrubPwd W:\EFI\Microsoft\Boot\
-} else { 
-Write-Output "$GrubEfiFileLoc is and $MsGrubEfiLoc are the same file" 
-}
-
-if (-not (Test-Path $MsGrubEfiLoc)) {
-Write-Output "$MsGrubEfiLoc not found! Coptying $gruppwd to W:\EFI\Microsoft\Boot\"
-cp $GrubPwd W:\EFI\Microsoft\Boot\
-}
-
 Write-Output "MD5 compare W:\EFI\Microsoft\Boot\*.efi" 
 
 $hashfile = "W:\hash.output.txt"
@@ -80,55 +35,101 @@ $hashfileout = $hashfilenew
 $hashfileout = $hashfile
         }
 
-Write-Output "Writing file to: $hashfileout"
+Write-Output "Writing file hashes to: $hashfileout"
 
-function md5hashefi() {
-$EfiFilePath = "W:\EFI\Microsoft\Boot\bootmgfww.efi"
+function MD5HashEfi() {
 $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+
+$EfiFilePath = "W:\EFI\Microsoft\Boot\bootmgfww.efi"
 $hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($EfiFilePath))).Replace("-","")
-
-Write-Output $hash
+Write-Output "$EfiFilePath MD%: $hash"
 Out-File -FilePath $hashfileout -InputObject $hash
-
 
 $EfiFilePath = "W:\EFI\Microsoft\Boot\bootmgfw.efi"
 $hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($EfiFilePath))).Replace("-","")
 
-Write-Output $hash
+Write-Output "$EfiFilePath MD%: $hash"
 Out-File -FilePath $hashfileout -InputObject $hash -Append
 
 
-$EfiFilePath = "$MsGrubEfiLoc"
+$EfiFilePath = "$GrubForMsEfiLoc"
 $hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($EfiFilePath))).Replace("-","")
 
-Write-Output $hash
+Write-Output "$EfiFilePath MD%: $hash"
 Out-File -FilePath $hashfileout -InputObject $hash -Append
-
 }
-
-md5hashefi
+MD5HashEfi
 
 function CompareHashFiles() {
-
 if((Get-FileHash $hashfile).hash  -ne (Get-FileHash $hashfilenew).hash) {
 write-output "EFI files are different replacing changed EFI files"
 
-if((Get-FileHash W:\EFI\Microsoft\Boot\bootmgfw.efi).hash  -ne (Get-FileHash  W:\EFI\Microsoft\Boot\bootmgfww.efi).hash) {
-cp W:\EFI\Microsoft\Boot\bootmgfw.efi W:\EFI\Microsoft\Boot\bootmgfww.efi
-}
+	write-output "Locate grub*.efi and path:"
+	$GrubEfiFileLoc = Get-ChildItem –Path W:\EFI -include grubx64.efi*,grubia32.efi* -Force -Recurse |
+		? FullName -notLike 'W:\EFI\Microsoft\Boot\grubx64.efi*' |
+			? FullName -notLike 'W:\EFI\Microsoft\Boot\grubia32.efi*' |
+				Get-ChildItem -File -Force |
+				select-object -Expand FullName
+#	$GrubEfiFileLoc 
 
-if((Get-FileHash $MsGrubEfiLoc).hash  -ne (Get-FileHash W:\EFI\Microsoft\Boot\bootmgfw.efi).hash) {
-cp $MsGrubEfiLoc W:\EFI\Microsoft\Boot\bootmgfw.efi
-}
+	$GrubPwd = Split-Path -Path "$GrubEfiFileLoc" -Parent
+	write-output "GRUB location: $GrubPwd"
 
-cp $hashfileout $hashfile
 
-} else {
-Write-output  "EFI files are the same"
-  }
+	if (Test-Path "$GrubPwd\grubia32.efi*") {
+	$GrubForMsEfiLoc = "W:\EFI\Microsoft\Boot\grubia32.efi"
+	write-output "GRUB EFI is 32bit using: $GrubForMsEfiLoc"
+	} else {
+	$GrubForMsEfiLoc = "W:\EFI\Microsoft\Boot\grubx64.efi"
+	write-output "GRUB EFI is 64bit using: $GrubForMsEfiLoc"
+	}
+
+	if((Get-FileHash $GrubEfiFileLoc).hash  -ne (Get-FileHash $GrubForMsEfiLoc).hash) {
+	Write-Output "$GrubEfiFileLoc is not the same as $GrubForMsEfiLoc"
+	cp $GrubPwd W:\EFI\Microsoft\Boot\
+	cp $GrubEfiFileLoc W:\EFI\Microsoft\Boot\bootmgfw.efi
+	} else { 
+	Write-Output "$GrubEfiFileLoc is and $GrubForMsEfiLoc are the same file" 
+    }
+
+	write-output "Locate grub*.efi and path:"
+
+	$GrubEfiFileLoc = Get-ChildItem –Path W:\EFI -include grubx64.efi*,grubia32.efi* -Force -Recurse |
+    ? FullName -notLike 'W:\EFI\Microsoft\Boot\grubx64.efi*' |
+        ? FullName -notLike 'W:\EFI\Microsoft\Boot\grubia32.efi*' |
+        Get-ChildItem -File -Force |
+            select-object -Expand FullName
+	$GrubEfiFileLoc 
+
+	$GrubPwd = Split-Path -Path "$GrubEfiFileLoc" -Parent
+	write-output "GRUB location: $GrubPwd"
+
+
+	if (Test-Path "$GrubPwd\grubia32.efi*") {
+	$GrubForMsEfiLoc = "W:\EFI\Microsoft\Boot\grubia32.efi"
+	write-output "GRUB EFI is 32bit using: $GrubForMsEfiLoc"
+	} else {
+	$GrubForMsEfiLoc = "W:\EFI\Microsoft\Boot\grubx64.efi"
+	write-output "GRUB EFI is 64bit using: $GrubForMsEfiLoc"
+	}
+
+	if((Get-FileHash $GrubEfiFileLoc).hash  -ne (Get-FileHash $GrubForMsEfiLoc).hash) {
+	Write-Output "$GrubEfiFileLoc is not the same as $GrubForMsEfiLoc"
+	cp $GrubPwd W:\EFI\Microsoft\Boot\
+	cp $GrubEfiFileLoc W:\EFI\Microsoft\Boot\bootmgfw.efi
+	} else { 
+	Write-Output "$GrubEfiFileLoc is and $GrubForMsEfiLoc are the same file, no need to update GRUB" 
+	}
+
+	cp $hashfileout $hashfile
+
+	} else {
+	Write-output  "EFI files are the same"
+	}
 
 }
 CompareHashFiles
+
 
 Write-Output "All should be well"
 pause
